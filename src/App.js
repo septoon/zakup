91ea79545c27e243939cf088287060168b5c0efb
Tigran <41443306+septoon@users.noreply.github.com>
2024-06-16 17:55:33 +0300

    redesign

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import Chef from './common/images/chef.png'
+import Bar from './common/images/bar.png'
+import Grill from './common/images/grill.png'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="flex flex-col justify-center items-center w-screen h-screen pl-2">
+      <div className='w-[80%] h-20 flex justify-start pl-5 items-center mb-5 bg-silver rounded-lg'>
+        <img src={Chef} className='h-5 mr-3' alt='kitchen'/>
+        Кухня
+      </div>
+      <div className='w-[80%] h-20 flex justify-start pl-5 items-center mb-5 bg-silver rounded-lg'>
+        <img src={Bar} className='h-5 mr-3' alt='bar'/>
+        Бар
+      </div>
+      <div className='w-[80%] h-20 flex justify-start pl-5 items-center bg-silver rounded-lg'>
+        <img src={Grill} className='h-5 mr-3' alt='grill'/>
+        Мангал
+      </div>
     </div>
   );
 }
---
+++ tailwind.config.js
@@ -0,0 +1,78 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    fontSize:{
+      sm: '0.8rem',
+      base: '1rem',
+      xl: '1.25rem',
+      '2xl': '1.563rem',
+      '3xl': '1.953rem',
+      '4xl': '2.441rem',
+      '5xl': '3.052rem',
+      'title': '26px'
+    },
+    fontWeight: {
+      thin: '100',
+      light: '300',
+      normal: '400',
+      medium: '500',
+      semibold: '600',
+      bold: '700',
+      extrabold: '800',
+      black: '900',
+    },
+    extend: {
+      fontFamily: {
+        pacifico: ['Pacifico', 'cursive'],
+        comfortaa: ['Comfortaa', 'sans-serif'],
+      },
+      backgroundImage: {
+        'shd': "url('/img/bg-shd.jpg')",
+        'menu': "url('/img/menu-icon.png')",
+        'main': "url('/img/bg-main.webp')",
+      },
+      backgroundPosition: {
+        bottom: 'bottom',
+        'bottom-4': 'center bottom 1rem',
+        center: 'center',
+        left: 'left',
+        'left-bottom': 'left bottom',
+        'left-top': 'left top',
+        right: 'right',
+        'right-bottom': 'right bottom',
+        'right-top': 'right top',
+        top: 'top',
+        'top-4': 'center top 1rem',
+      },
+      boxShadow: {
+        '3xl': '0 35px 60px 5px rgba(0, 0, 0, .9)',
+      },
+      spacing: {
+        'half-screen': '55vh',
+        'screen-20': '120vh',
+        'main-btn': '4dvh'
+      }
+    },
+    colors: {
+      'dark': "#1d1d1d",
+      'transparent': 'transparent',
+      'white': '#ffffff',
+      'purple': '#3f3cbb',
+      'midnight': '#121063',
+      'metal': '#565584',
+      'tahiti': '#3ab7bf',
+      'silver': '#ecebff',
+      'bubble-gum': '#ff77e9',
+      'bermuda': '#78dcca',
+      'orange': '#FFA500',
+      'light-gray': '#D3D3D3',
+      'lightSlate-gray': '#778899',
+      'DimGray': '#696969',
+      'veryDark-gray': '#272822',
+    }
+  },
+  plugins: [],
+}
