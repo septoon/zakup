8cf4b8bbcddc77b24813cc257dc80499345e6e50
Tigran <41443306+septoon@users.noreply.github.com>
2024-06-17 13:08:11 +0300

    dark mode

--- src/App.js
+++ src/App.js
@@ -9,21 +9,21 @@ import { Link } from 'react-router-dom';
 function App() {
   return (
     <div className="flex flex-col justify-center items-center w-screen h-screen pl-2">
-      <Link to='/kitchen' className='w-[80%] h-20 flex justify-start pl-5 items-center mb-5 bg-silver rounded-lg'>
+      <Link to='/kitchen' className='w-[80%] h-20 flex justify-start pl-5 items-center mb-5 bg-silver dark:bg-darkGray rounded-lg'>
         <img src={Chef} className='h-5 mr-3' alt='kitchen'/>
-        Кухня
+        <span className='dark:text-white'>Кухня</span>
       </Link>
-      <Link to='/bar' className='w-[80%] h-20 flex justify-start pl-5 items-center mb-5 bg-silver rounded-lg'>
+      <Link to='/bar' className='w-[80%] h-20 flex justify-start pl-5 items-center mb-5 bg-silver dark:bg-darkGray rounded-lg'>
         <img src={Bar} className='h-5 mr-3' alt='bar'/>
-        Бар
+        <span className='dark:text-white'>Бар</span>
       </Link>
-      <Link to='/mangal' className='w-[80%] h-20 flex justify-start pl-5 items-center mb-5 bg-silver rounded-lg'>
+      <Link to='/mangal' className='w-[80%] h-20 flex justify-start pl-5 items-center mb-5 bg-silver dark:bg-darkGray rounded-lg'>
         <img src={Grill} className='h-5 mr-3' alt='grill'/>
-        Мангал
+        <span className='dark:text-white'>Мангал</span>
       </Link>
-      <Link to='/house' className='w-[80%] h-20 flex justify-start pl-5 items-center bg-silver rounded-lg'>
+      <Link to='/house' className='w-[80%] h-20 flex justify-start pl-5 items-center bg-silver dark:bg-darkGray rounded-lg'>
         <img src={House} className='h-5 mr-3' alt='household'/>
-        Хоз товары
+        <span className='dark:text-white'>Хоз товары</span>
       </Link>
     </div>
   );
